a2bb27f52b6e5a2110d7a639844f1e6f3c2e0207
oMarcosAugusto <marcoosaugusto@hotmail.com>
2023-10-23 11:56:24 +0100

    Aplicado metodo meuImprimir e aplicado no codigo

--- lambda/src/main/java/foreach/lambda/ForEach.java
+++ lambda/src/main/java/foreach/lambda/ForEach.java
@@ -18,7 +18,18 @@ public class ForEach {
 //        });        OU(||)
           aprovados.forEach(nome -> System.out.println("* " + nome));
 
-        System.out.println("\nMethod Reference: ");
+        System.out.println("\nMethod Reference #1: ");
           aprovados.forEach(System.out::println);   // passa o valor da lista direto pro print
+
+        System.out.println("\nLambda #02: ");
+        //          Agora podemos chamar o metodo meuImprimir para passar os dados
+        aprovados.forEach(nome -> meuImprimir(nome));
+
+        System.out.println("\nMethod Reference #2: ");
+        aprovados.forEach(ForEach::meuImprimir);   // passa o valor da lista direto pro print
+
+    }
+    static void meuImprimir(String nome) {
+        System.out.println("Oi meu nome e " + nome);
     }
 }

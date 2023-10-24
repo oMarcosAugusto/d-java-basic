a288c69419723ccd117c32ebba216e3315e3af40
oMarcosAugusto <marcoosaugusto@hotmail.com>
2023-10-24 21:42:45 +0100

    Realizado desafio de MAP

---
+++ streamAPI/src/main/java/stream/desafio/Desafio.java
@@ -0,0 +1,16 @@
+package stream.desafio;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Desafio {
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        nums.stream()
+                .map(Integer::toBinaryString) // 1. Número para string binária
+                .map(str -> new StringBuilder(str).reverse().toString()) // 2. Reverter a string
+                .map(str -> Integer.parseInt(str, 2)) // 3. Converter de volta para inteiro
+                .forEach(System.out::println); // Imprimir o resultado
+    }
+}

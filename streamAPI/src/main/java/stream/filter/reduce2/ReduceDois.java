4f1d1270dc223f1467286b7ad11e0b50496fb41b
oMarcosAugusto <marcoosaugusto@hotmail.com>
2023-10-27 21:49:17 +0100

    Exercicio ReduceDois realizado

---
+++ streamAPI/src/main/java/stream/filter/reduce2/ReduceDois.java
@@ -0,0 +1,29 @@
+package stream.filter.reduce2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class ReduceDois {
+    public static void main(String[] args) {
+
+        Aluno a1 = new Aluno("Ana", 7.1);
+        Aluno a2 = new Aluno("Luna", 6.1);
+        Aluno a3 = new Aluno("Gui", 8.1);
+        Aluno a4 = new Aluno("Gabi", 10);
+
+        List<Aluno> alunos = Arrays.asList(a1, a2, a3, a4);
+
+        Predicate<Aluno> aprovado = a -> a.nota >= 7;
+        Function<Aluno, Double> nota = a -> a.nota;
+        BinaryOperator<Double> somatorio = (a,b) -> a + b;
+
+        alunos.stream()
+                .filter(aprovado)
+                .map(nota)
+                .reduce(somatorio) // soma os valores dos aprovados!!
+                .ifPresent(System.out::println);
+    }
+}

621184f2dba3c1f33c288244a726fe0755a2ec40
oMarcosAugusto <marcoosaugusto@hotmail.com>
2023-10-31 10:12:10 +0000

    Ajustado Campo e aplicado testes em CampoTeste

--- projeto-campoMinado/src/modelo/Campo.java
+++ projeto-campoMinado/src/modelo/Campo.java
@@ -14,8 +14,30 @@ public class Campo {
 	
 	private List<Campo> vizinhos = new ArrayList<>();
 	
-	Campo(int linha, int coluna) {
+	public Campo(int linha, int coluna) {
 		this.linha = linha;
 		this.coluna = coluna;
+	}
+	
+	public boolean adicionarVizinho(Campo vizinho) {
+		
+		boolean linhaDiferente = linha != vizinho.linha;
+		boolean colunaDiferente = coluna != vizinho.coluna;
+		boolean diagonal = linhaDiferente && colunaDiferente;
+		
+		int deltaLinha = Math.abs(linha - vizinho.linha);
+		int deltaColuna = Math.abs(coluna - vizinho.coluna);
+		int deltaGeral = deltaColuna + deltaLinha;
+		
+		if (deltaGeral == 1 && !diagonal) {
+			vizinhos.add(vizinho);
+			return true;
+		} else if (deltaGeral == 2 && diagonal) {
+			vizinhos.add(vizinho);
+			return true;
+		} else {
+			return false;
+		}
+		
 	}
 }
